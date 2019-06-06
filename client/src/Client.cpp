7c43caebf6f1c055126f26fbe58441bf006ccc9b
Callum <callum@ischus.co.uk>
2019-06-06 19:44:12 +0100

    Add Client class

---
+++ client/src/Client.cpp
@@ -0,0 +1,1 @@
+#include <AjaxCatalyst/Client/Client.h>
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,19 +1,12 @@
 #include <SFML/Window.hpp>
+#include <AjaxCatalyst/Client/Client.h>
 
 int main(int argc, char** argv)
 {
-	sf::Window window(sf::VideoMode(800, 600), "AjaxCatalystClient");
-
-	while (window.isOpen())
-	{
-		sf::Event e;
-
-		while (window.pollEvent(e))
-		{
-			if (e.type == sf::Event::Closed)
-				window.close();
-		}
-	}
+	AjaxCatalyst::Client* client = new AjaxCatalyst::Client();
+	sf::Clock clock;
+	sf::Time lag = sf::Time::Zero;
+	const sf::Time frameLimit = sf::seconds(1.0f / 60.0f); // 60 frames-per-second
 
 	return EXIT_SUCCESS;
 }
